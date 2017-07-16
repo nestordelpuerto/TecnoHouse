8369a82c90a0041c869218546c3106617aca97a2
nestordelpuerto <david.gomez211019@gmail.com>
2017-07-16 00:06:57 -0400

    Prueba de sensores de temperatura y humedad

--- TecnoHouse.ino
+++ TecnoHouse.ino
@@ -1,10 +1,20 @@
-#include <LiquidCrystal.h>
+#include "DHT.h" //agregamos la libreria DHT del sensor de humedad y temperatura
+#define DHTPIN 2 //definimos el pin de conexion del sensor DHT
+#define DHTTYPE DHT22 //definimos el modelo del sensor
+DHT dht (DHTPIN,DHTTYPE); //creamos una variable dht para el sensor de humedad y temperatura
+
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600); //inicializamos el monitor serial
+  dht.begin(); //iniciamos el sensor
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  float h = dht.readHumidity(); //leemos la humedad
+  float t = dht.readTemperature(); //leemos de temperatura
+  Serial.println ("Humedad: "); 
+  Serial.println (h);
+  Serial.println ("Temperatura: ");
+  Serial.println (t);
+  delay(2000);
 }
+
